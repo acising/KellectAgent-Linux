16e85e64b05ccc794c38c12b0937cadd029a31c6
zhuzhiling <917637197@qq.com>
2023-04-09 10:43:51 +0800

    adjust some files

--- src/include/main.h
+++ src/include/main.h
@@ -6,8 +6,7 @@
 #ifndef KELLECT_LINUX_MAIN_H
 #define KELLECT_LINUX_MAIN_H
 
-int test_minimal();
-int test_demo(Args args);
+int test_all(Args args);
 int test_file(Args args);
 int test_process(Args args);
 int test_network(Args args);
--- src/trace/all/all.bpf.c
+++ src/trace/all/all.bpf.c
@@ -7,7 +7,7 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_core_read.h>
 #include <bpf/bpf_tracing.h>
-#include "../../include/demo.h"
+#include "../../include/all.h"
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
